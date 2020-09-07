66c47e526ddc2485fd812cdbf2384d06eee941c1
Mohammad Ahmed <yahmed786222@gmail.com>
2020-09-07 21:38:33 +0530

    Added react menu components to my first React Application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap'
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
